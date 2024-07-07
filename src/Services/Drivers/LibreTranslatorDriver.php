f0343e466a910eeb78038c958d8fcc0b8b76ee52
Anthony Ogundipe <diltony@gmail.com>
2024-07-07 19:53:22 +0100

    Updated files

--- src/Services/Drivers/LibreTranslatorDriver.php
+++ src/Services/Drivers/LibreTranslatorDriver.php
@@ -79,7 +79,7 @@ class LibreTranslatorDriver
 
     private function translateText($text, $target, &$response)
     {
-        sleep(5);
+        sleep(6);
 
         $url = "https://libretranslate.com/translate";
         $data = ['q' => $text, 'source' => $this->currentLocale, //auto is failing drastically
